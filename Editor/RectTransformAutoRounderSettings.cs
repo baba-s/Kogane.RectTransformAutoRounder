81fb71bbec3c8e4603f7c8beba10b53a6af38ba9
baba_s <shotababa1206@gmail.com>
2020-10-05 19:28:06 +0900

    first commit

---
+++ Editor/RectTransformAutoRounderSettings.cs
@@ -0,0 +1,49 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+	/// <summary>
+	/// Preferences における設定を管理する ScriptableObject
+	/// </summary>
+	internal sealed class RectTransformAutoRounderSettings : ScriptableObjectForPreferences<RectTransformAutoRounderSettings>
+	{
+		//================================================================================
+		// 定数
+		//================================================================================
+		private static readonly string PACKAGE_NAME        = "UniRectTransformAutoRounder";
+		private static readonly bool   DEFAULT_ENABLED_LOG = false;
+		private static readonly string DEFAULT_LOG_FORMAT  = $"[{PACKAGE_NAME}] シーンの保存にかかった時間：{{0}} 秒";
+
+		//================================================================================
+		// 変数(static)
+		//================================================================================
+		[SerializeField] private bool   m_enabledLog = DEFAULT_ENABLED_LOG;
+		[SerializeField] private string m_logFormat  = DEFAULT_LOG_FORMAT;
+
+		//================================================================================
+		// プロパティ
+		//================================================================================
+		public bool   EnabledLog => m_enabledLog;
+		public string LogFormat  => m_logFormat;
+
+		//================================================================================
+		// 関数(static)
+		//================================================================================
+		[SettingsProvider]
+		private static SettingsProvider SettingsProvider()
+		{
+			return CreateSettingsProvider
+			(
+				settingsProviderPath: $"Kogane/{PACKAGE_NAME}",
+				onGUIExtra: so =>
+				{
+					if ( !GUILayout.Button( "Reset to Default" ) ) return;
+
+					so.FindProperty( nameof( m_enabledLog ) ).boolValue  = DEFAULT_ENABLED_LOG;
+					so.FindProperty( nameof( m_logFormat ) ).stringValue = DEFAULT_LOG_FORMAT;
+				}
+			);
+		}
+	}
+}
